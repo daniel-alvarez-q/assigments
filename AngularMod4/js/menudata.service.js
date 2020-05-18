fc7ad926ab727581f07a90e3379b92ae78fe096c
Daniel J Alvarez <daniel.alvarez.q@gmail.com>
2020-05-19 00:03:04 +0200

    Initial configuration and set-up for Angular's fourth assigment

---
+++ AngularMod4/js/menudata.service.js
@@ -0,0 +1,17 @@
+(function(){
+'use strict';
+
+angular.module('data').service('MenuDataService', MenuDataService);
+
+function MenuDataService (){
+    var MDService = this;
+
+    MDService.getAllCategories  = function (){
+        //
+    }
+
+    MDService.getItemsForCategories = function (){
+        //
+    }
+}
+})();
