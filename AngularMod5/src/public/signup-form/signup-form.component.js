901b9c7c06ac90f40289d0fd49aa750b06477567
Daniel J Alvarez <daniel.alvarez.q@gmail.com>
2020-05-22 15:22:08 +0200

    Initial angular setup for signin form

--- AngularMod5/src/public/signup-form/signup-form.component.js
+++ AngularMod5/src/public/signup-form/signup-form.component.js
@@ -2,7 +2,22 @@
 "use strict";
 
 angular.module('public').component('signupForm', {
-    templateUrl: 'src/public/signup-form/signup-form.html'
+    templateUrl: 'src/public/signup-form/signup-form.html',
+    controller: formController,
 });
+
+
+function formController (){
+    var ctrl = this;
+    var user = {};
+    ctrl.signUp = function (){
+        user.firstName = ctrl.firstName;
+        user.lastName = ctrl.lastName;
+        user.email = ctrl.email;
+        user.phone = ctrl.phone;      
+        console.log(user);
+    };
+
+}
 
 })();
